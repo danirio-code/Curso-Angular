727f447f3b61cd8ec23f0886ccf420d98f7219f4
Daniel Rio <daniel.rio@w2m.com>
2024-03-20 17:13:08 +0100

    Conceptos básicos angular + Prueba configuración linter w2m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'angular-app'
 }
+
+// Genera un código TypeScript que no sea del todo optimo para ver que funciona SonarQube
