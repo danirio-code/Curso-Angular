5215904e5d0aef9cc62f1cd8ae69082b4f19b54e
Daniel Rio <daniel.rio@w2m.com>
2024-03-21 14:26:50 +0100

    Acabada práctica básica calculadora

--- calculadora-app/README.md
+++ calculadora-app/README.md
@@ -1,5 +1,7 @@
 # CalculadoraApp
 
+![captura](./src/assets/captura.png)
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.11.
 
 ## Development server
--- calculadora-app/src/app/app.component.ts
+++ calculadora-app/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'Mini Calculadora'
+
+  numero1 = 0
+
+  numero2 = 0
+
+  resultado = 0
+
+  sumar(): void {
+    this.resultado = this.numero1 + this.numero2
+  }
+
+  restar(): void {
+    this.resultado = this.numero1 - this.numero2
+  }
+
+  multiplicar(): void {
+    this.resultado = this.numero1 * this.numero2
+  }
 }
