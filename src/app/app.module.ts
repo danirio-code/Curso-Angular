97e792c2d25d4130b0349156262c2470a5f58cb8
Daniel Rio <daniel.rio@w2m.com>
2024-03-21 13:14:27 +0100

    Interpolacion + Property Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component'
 import { EmpleadosComponent } from './empleados/empleados.component'
 import { EmpleadoComponent } from './empleado/empleado.component'
@@ -13,6 +14,7 @@ import { EmpleadoComponent } from './empleado/empleado.component'
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core'
   styleUrls: ['./empleado.component.css'],
 })
 export class EmpleadoComponent {
+  nombre = 'Dani'
 
+  apellido = 'Río'
+
+  edad = 22
+
+  deshabilitacionInput = false
+
+  usuarioRegistrado = false
+
+  textoDeRegistro = 'No hay nadie registrado'
+
+  empresa = 'Google'
+
+  // getRegistroUsuario(): void {
+  //   this.usuarioRegistrado = false
+  // }
+
+  showRegisteredText(event: Event): void {
+    if ((<HTMLInputElement>event.target).value === 'true') {
+      this.textoDeRegistro = 'El usuario se ha registrado correctamente'
+    } else {
+      this.textoDeRegistro = 'No hay nadie registrado'
+    }
+  }
 }
