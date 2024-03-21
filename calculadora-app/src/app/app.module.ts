d55d9508f0356b567f0e2a6cbace7f00a6eb7b2f
Daniel Rio <daniel.rio@w2m.com>
2024-03-21 13:39:44 +0100

    aplicado linter

--- calculadora-app/src/app/app.module.ts
+++ calculadora-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
