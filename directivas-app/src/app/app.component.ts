0da7b4589a1c377b5e50aa2506594a6b4650e0c9
Daniel Rio <daniel.rio@w2m.com>
2024-03-21 16:29:27 +0100

    Aprendiendo directivas ngIf y ngFor

--- directivas-app/src/app/app.component.ts
+++ directivas-app/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'directivas-app'
+  titulo = 'Registro de Usuario'
+
+  nombre = ''
+
+  apellido = ''
+
+  mensaje = ''
+
+  registrado = false
+
+  entradas: { titulo: string }[]
+
+  constructor() {
+    this.entradas = [
+      {
+        titulo: 'Python, cada día más popular',
+      },
+      {
+        titulo: 'Java, presente desde hace más de 20 años',
+      },
+      {
+        titulo: 'JavaScript, cada vez más funcional',
+      },
+      {
+        titulo: 'Kotlin, potencia para tus apps',
+      },
+      {
+        titulo: '¿Dónde quedó pascal?',
+      },
+    ]
+  }
+
+  registrarUsuario(): void {
+    this.registrado = true
+    this.mensaje = 'Usuario registrado con éxito'
+  }
 }
